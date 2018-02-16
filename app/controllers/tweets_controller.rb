ee6a71eaed1fd2d316ae688a3486b538d2fd366f
Nicolas Roulleau <nicolas.roulleau@gmail.com>
2018-02-16 11:20:07 +0100

    new method

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,22 +1,37 @@
 class TweetsController < ApplicationController
-  
-   def new
-  	@tweet = Tweet.new
+
+  def home
+  end
+
+  def tweet
+    SendTweet.new(params[:tweet][:message]).perform
+  end
+
+end
+
+
+
+=begin AUTRE METHODE
+
+  def new
+    @tweet = Tweet.new
   end
 
   def create
-  	@tweet = Tweet.new(tweet_params)
-  	if @tweet.save
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
       flash[:success] = "Votre tweet a bien été twitté !" 
       SendTweet.new("#{@tweet.content}").perform
       redirect_to root_path 
-  	else render 'new'
-  	end
+    else render 'new'
+    end
   end
 
   private
-  	def tweet_params
+    def tweet_params
       params.permit(:content)
-  	end
+    end
 
-end
+=end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
 root 'tweets#new'
-resources :tweets
+post '/', to: 'tweets#tweet'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
